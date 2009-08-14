234ea060a52405c2273d85886e5b7bb60faa66ef
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-08-14 18:47:33 +0000

    apply css updates from arabic/persian to hebrew landing page
    git-svn-id: https://svn.mozilla.org/projects/mozilla.com/trunk/he@49203 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/header-landing-page-l10n.inc.php
+++ includes/header-landing-page-l10n.inc.php
@@ -1,5 +1,14 @@
 <?php
 
+        // We are right-to-left
+        $textdir = 'rtl';
+
+        // We are the landing page
+        $landing_page = true;
+
+        // Include RTL support helper
+        require_once "{$config['file_root']}/{$lang}/includes/rtl-support.php";
+
         // Include the global header.  All locales will include this.
         require "{$config['file_root']}/includes/header-landing-page-l10n.inc.php";
 
