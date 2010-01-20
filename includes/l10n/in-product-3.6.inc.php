6f7effc6944aeccd4425d90651aafb5a420d8835
steven@silverorange.com <steven@silverorange.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2010-01-20 21:05:46 +0000

    Merging Firefox 3.6 branch into trunk (-r52372:60557)
    git-svn-id: https://svn.mozilla.org/projects/mozilla.com/trunk/he@60623 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ includes/l10n/in-product-3.6.inc.php
@@ -0,0 +1,47 @@
+
+<div id="main-content">
+
+  <div class="sub-feature" id="personas">
+
+    <h3>בחר את הפרסונה שלך.</h3>
+    <p id="try">עמוד עלי כדי לנסות, לחיצה כדי להפוך לקבוע</p>
+
+    <a href="http://www.getpersonas.com/en-US/gallery/" id="personas-image-link">Personas</a>
+    <iframe
+        src="http://www.getpersonas.com/en-US/external/mozilla/firstrun.php"
+        width="320"
+        height="200">
+    </iframe>
+
+    <ul id="personas-link" class="link">
+      <li>
+          <a href="http://www.getpersonas.com/en-US/gallery/" id="see-all-personas">הצג הכל (מעל 30,000)</a>
+      </li>
+    </ul>
+</div>
+
+  <div class="sub-feature" id="connect">
+    <h3>השאר מחובר</h3>
+    <ul class="link">
+      <li id="connect-twitter"><a href="http://twitter.com/firefox">עקוב אחרינו ב־Twitter</a></li>
+      <li id="connect-facebook"><a href="http://www.facebook.com/Firefox">היה אוהד ב־Facebook</a></li>
+      <li id="connect-blog"><a href="http://blog.mozilla.com/">קרא את הבלוג שלנו</a></li>
+    </ul>
+  </div>
+
+  <div class="sub-feature" id="personalize"><div>
+    <h3>דרכים נוספות להתאמה אישית</h3>
+    <p>התאם את Firefox לצורה בה תרצה לגלוש ברשת עם אלפי תוספות חופשיות.</p>
+    <ul class="link">
+      <li>
+          <a href="https://addons.mozilla.org/<?=$lang?>/firefox/">סייר בתוספות</a>
+      </li>
+    </ul>
+  </div></div>
+
+  <p id="sub-links">
+      <a href="/<?=$lang?>/firefox/features/" id="features-link">תכונות נוספות של Firefox 3.6</a>
+      <a href="http://support.mozilla.com" id="support-link">תמיכה ב־Firefox</a>
+  </p>
+
+</div>
