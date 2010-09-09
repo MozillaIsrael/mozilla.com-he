2de06fd2b860baf983d456fc63dded8f943569c3
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2010-09-09 14:51:29 +0000

    l10n: typo fixes on subs
    git-svn-id: https://svn.mozilla.org/projects/mozilla.com/trunk/he@74203 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/videos/sub-fx4-direct2d.php
+++ includes/l10n/videos/sub-fx4-direct2d.php
@@ -6,7 +6,7 @@ $sub[3]  = "And, I want to show you the difference between Firefox 4 with and wi
 $sub[4]  = "So here you have a little animation running into Firefox 4 without hardware acceleration.";
 $sub[5]  = "So it's not that smooth, it's running 12 frames per second.";
 $sub[6]  = "And now I'm going to run the same animation, but this time, into Firefox 4 with hardware acceleration.";
-$sub[7]  = "And, as you can see, it's way way smoother, and well, you know, it's also way faster at 90 frames per second.";
+$sub[7]  = "And, as you can see, it's way way smoother, and well, you know, it's also way faster at 91 frames per second.";
 $sub[8]  = "Thank You.";
 
 ?>
