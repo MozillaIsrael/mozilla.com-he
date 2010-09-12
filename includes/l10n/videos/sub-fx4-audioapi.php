b36c2f58413045320904acc29482e4d60f4fba62
tomer@gmx.net <tomer@gmx.net@4eb1ac78-321c-0410-a911-ec516a8615a5>
2010-09-12 12:05:13 +0000

    Beta 5 updates
    git-svn-id: https://svn.mozilla.org/projects/mozilla.com/trunk/he@74322 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/videos/sub-fx4-audioapi.php
+++ includes/l10n/videos/sub-fx4-audioapi.php
@@ -1,19 +1,19 @@
 
 <?php
 
-$sub[1]  = "HTML5 gives us the &lt;audio&gt; element.";
-$sub[2]  = "<em>What if we could do more than just press play?</em>";
-$sub[3]  = "Expose Raw data as it plays.";
-$sub[4]  = "Display audio sound data with &lt;canvas&gt;";
-$sub[5]  = "<em>-0.008158281445503235… What do these numbers really look like?</em>";
-$sub[6]  = "Apply a low pass filter to the audio.";
-$sub[7]  = "<em>Read audio, apply filter, and output.</em>";
-$sub[8]  = "Calculate Audio spectrum (FFT) in JavaScript.";
-$sub[9]  = "Playing with spectrum, visualizing sound.";
-$sub[10] = "Analyze audio: beat detection in JavaScript";
-$sub[11] = "Signal, FFT, Beat as 2D visualizations.";
-$sub[12] = "Audio visualization: 2D and 3D &lt;canvas&gt;";
-$sub[13] = "Demo by Dave Humphrey, Al MacDonald, Corban Brook, Charles Cliffe, Thomas Sturm, Yury Delendik.";
-$sub[14] = "Music: Revolve by His Boy Elroy (Licensed under Creative Commons Noncommercial Sampling Plus)";
+$sub[1]  = "HTML5 מביא לנו את האלמנט &lt;audio&gt;.";
+$sub[2]  = "<em>מה אם נוכל לעשות יותר מרק ללחוץ על כפתור הניגון?</em>";
+$sub[3]  = "חשיפת מידע גולמי במהלך הניגון.";
+$sub[4]  = "הצגת מידע הצליל עם &lt;canvas&gt;";
+$sub[5]  = "<em>-0.008158281445503235… כיצד באמת נראים המספרים הללו?</em>";
+$sub[6]  = "החלת מסנן המעביר תדרים נמוכים על האודיו.";
+$sub[7]  = "<em>קריאת האודיו, החלת המסנן, ופלט הצליל.</em>";
+$sub[8]  = "חישוב ספקטרום האודיו (FFT) ב־JavaScript.";
+$sub[9]  = "משחקים עם הספקטרום, הפיכת הצליל לויזואלי.";
+$sub[10] = "ניתוח אודיו: זיהוי פעימות ב־JavaScript";
+$sub[11] = "אות, FFT, ופעימות בתצוגה די מימדית.";
+$sub[12] = "ניתוח אודיו: &lt;canvas&gt; בדו מימד ובתלת מימד";
+$sub[13] = "הדגמה של Dave Humphrey, Al MacDonald, Corban Brook, Charles Cliffe, Thomas Sturm, Yury Delendik.";
+$sub[14] = "מוסיקה: Revolve by His Boy Elroy (ברשיון Creative Commons Noncommercial Sampling Plus)";
 
 ?>
--- includes/l10n/videos/sub-fx4-direct2d.php
+++ includes/l10n/videos/sub-fx4-direct2d.php
@@ -1,12 +1,12 @@
 <?php
 
-$sub[1]  = "Hi, this is Paul Rouget from Mozilla.";
-$sub[2]  = "Today, I would like to show you Firefox 4, the new Beta, which comes with hardware acceleration.";
-$sub[3]  = "And, I want to show you the difference between Firefox 4 with and without hardware acceleration.";
-$sub[4]  = "So here you have a little animation running into Firefox 4 without hardware acceleration.";
-$sub[5]  = "So it's not that smooth, it's running 12 frames per second.";
-$sub[6]  = "And now I'm going to run the same animation, but this time, into Firefox 4 with hardware acceleration.";
-$sub[7]  = "And, as you can see, it's way way smoother, and well, you know, it's also way faster at 91 frames per second.";
-$sub[8]  = "Thank You.";
+$sub[1]  = "שלום, אני פאול רוג'ט מ־Mozilla.";
+$sub[2]  = "היום אני רוצה להציג לכם את גרסת הבטא החדשה של Firefox 4 המגיעה עם האצת חומרה.";
+$sub[3]  = "וגם להראות לכם את ההבדל של Firefox 4 עם ובלי האצת חומרה.";
+$sub[4]  = "להלן אנימציה קטנה שרצה על Firefox 4 ללא האצת חומרה.";
+$sub[5]  = "התמונה לא כל כך חלקה, היא מוצגת בקצב של 12 שקופיות בשנייה.";
+$sub[6]  = "כעת אמח הולך להריץ את אותה אנימציה, אבל הפעם על Firefox 4 עם האצת חומרה.";
+$sub[7]  = "כפי שניתן לראות, האנימציה הרבה יותר חלקה, וגם היא מהירה יותר בקצב של 91 שקופיות בשנייה.";
+$sub[8]  = "תודה לכם.";
 
 ?>
