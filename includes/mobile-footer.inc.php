3d2363c1db657f750198b578ec9a46cde5860fcf
steven@silverorange.com <steven@silverorange.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2013-06-07 00:16:45 +0000

    Remove unused files for Bug 875883
    git-svn-id: https://svn.mozilla.org/projects/mozilla.com/trunk/he@116741 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/mobile-footer.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-    // Include the global footer.  All locales will include this.
-    require "{$config['file_root']}/includes/mobile-footer.inc.php";
-    echo $footer;
-?>
-
--- includes/mobile-header.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-    // Include the global header.  All locales will include this.
-    require "{$config['file_root']}/includes/mobile-header.inc.php";
-    echo $header;
-?>
